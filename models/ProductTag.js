a50f1d61903679cc6593da5f9ff39a2c8516ef96
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-03-27 10:52:40 -0600

    added ids to ProductTag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,29 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    // id columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // product_id column
+    product_id: {
+      type: DataTypes.INTEGER,
+      refrences: {
+        model:'product',
+        key: 'id'
+      }
+    },
+    // tag_id column
+    tag_id: {
+      type: Datatypes.INTEGER,
+      refrences: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
